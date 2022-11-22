d7d30c14222566fb604313a2520f3dfcacdd552b
Manuela Garcia <manuela.garcia@bosonit.com>
2022-11-22 12:41:01 +0100

    create new module

---
+++ src/app/counter.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css'],
+})
+export class ContadorComponent {
+  public title: string = 'Contador App';
+  public base: number = 5;
+  public number: number = 10;
+  acumular(value: number) {
+    this.number += value;
+  }
+}
