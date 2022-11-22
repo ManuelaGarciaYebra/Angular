12ca453a6e2664efe9aa1292ed225370ad2b4e4c
Manuela Garcia <manuela.garcia@bosonit.com>
2022-11-22 18:05:13 +0100

    create deleted method

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  styleUrls: ['./listado.component.css'],
 })
-export class ListadoComponent {
 
+
+
+export class ListadoComponent {
+  public hero: string[] = ['Ironman', 'Batman', 'Spiderman', 'Hulk', 'Thor'];
+  public deletedHero: string= '';
+  deleteHero() {
+    this.deletedHero = this.hero.pop() || '';
+    //por defecto nos puede dar string o undefined, ts nos dará error de tipado y debemos indicarle que puede resultar ese tipo tb
+  }
 }
